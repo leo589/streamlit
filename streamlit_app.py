81d86ca51a909dbfa9dbf15f2769e141605dbdba
Ijika yuto <39280337+leo589@users.noreply.github.com>
2024-05-28 13:49:37 -0300

    feat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,22 @@
 import streamlit as st
-import socketio
-import eventlet
-from threading import Thread
-
-# Inicializa o servidor Socket.IO
-sio = socketio.Server()
-app = socketio.WSGIApp(sio)
 
 # Dicionário para armazenar os usuários registrados
 registered_users = {}
 
-# Função para processar a conexão de um novo cliente
-@sio.on('connect')
-def connect(sid, environ):
-    print('Client connected:', sid)
-
-# Função para processar a desconexão de um cliente
-@sio.on('disconnect')
-def disconnect(sid):
-    print('Client disconnected:', sid)
-
-# Função para lidar com a autenticação do usuário
-@sio.on('login')
-def login(sid, data):
-    username = data['username']
-    password = data['password']
-    if username in registered_users and registered_users[username] == password:
-        sio.emit('login_response', {'success': True}, room=sid)
-    else:
-        sio.emit('login_response', {'success': False, 'message': 'Usuário ou senha incorretos'}, room=sid)
-
-# Função para lidar com o registro de novos usuários
-@sio.on('register')
-def register(sid, data):
-    username = data['username']
-    password = data['password']
+# Função para registrar um novo usuário
+def register_user(username, password):
     if username not in registered_users:
         registered_users[username] = password
-        sio.emit('register_response', {'success': True}, room=sid)
+        return True
     else:
-        sio.emit('register_response', {'success': False, 'message': 'Nome de usuário já registrado'}, room=sid)
+        return False
 
-# Função para enviar mensagens entre os usuários
-@sio.on('send_message')
-def send_message(sid, data):
-    recipient = data['recipient']
-    message = data['message']
-    sio.emit('receive_message', {'sender': sid, 'message': message}, room=recipient)
-
-# Função para executar o servidor Socket.IO
-def run_server():
-    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
-
-# Inicia o servidor em uma thread separada
-server_thread = Thread(target=run_server)
-server_thread.start()
+# Função para autenticar um usuário
+def authenticate_user(username, password):
+    if username in registered_users and registered_users[username] == password:
+        return True
+    else:
+        return False
 
 # Interface Streamlit
 st.title("Chat em Tempo Real")
@@ -63,30 +25,27 @@ st.title("Chat em Tempo Real")
 option = st.sidebar.radio("Escolha uma opção:", ("Login", "Registro"))
 username = st.text_input("Nome de Usuário")
 password = st.text_input("Senha", type="password")
-if option == "Login":
-    if st.button("Entrar"):
-        sio.emit('login', {'username': username, 'password': password})
-        response = sio.get_sid_namespace().wait_for_callbacks(timeout=5)
-        if response and response[0]['name'] == 'login_response' and response[0]['args'][0]['success']:
-            st.success("Login bem-sucedido!")
-        else:
-            st.error("Usuário ou senha incorretos")
-elif option == "Registro":
+if option == "Registro":
     if st.button("Registrar"):
-        sio.emit('register', {'username': username, 'password': password})
-        response = sio.get_sid_namespace().wait_for_callbacks(timeout=5)
-        if response and response[0]['name'] == 'register_response' and response[0]['args'][0]['success']:
+        if register_user(username, password):
             st.success("Registro bem-sucedido!")
         else:
             st.error("Nome de usuário já registrado")
+elif option == "Login":
+    if st.button("Entrar"):
+        if authenticate_user(username, password):
+            st.session_state.logged_in = True
+            st.success("Login bem-sucedido!")
+        else:
+            st.error("Usuário ou senha incorretos")
 
 # Componentes para envio e recebimento de mensagens
-if st.session_state.logged_in:
+if st.session_state.get("logged_in", False):
     st.subheader("Conversa")
     recipient = st.selectbox("Enviar mensagem para:", list(registered_users.keys()))
     message = st.text_input("Mensagem")
     if st.button("Enviar"):
-        sio.emit('send_message', {'recipient': recipient, 'message': message})
+        st.success(f"Mensagem enviada para {recipient}: {message}")
 
 # Lógica para mudar para uma conversa diferente
 change_conversation = st.button("Mudar de Conversa")
